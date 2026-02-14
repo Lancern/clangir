9d1422ecff48a7977652792a9262b725ad182583
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2026-02-13 19:16:18 -0800

    [CIR] Add typeinfo test header for vtable emission tests
    Add typeinfo header to CIR CodeGen test directory and update
    vtable-available-externally.cpp to use system include style for
    typeinfo.
    
    Co-authored-by: Tommy McMichen <tommymcmichen@fb.com>

--- clang/test/CIR/CodeGen/vtable-available-externally.cpp
+++ clang/test/CIR/CodeGen/vtable-available-externally.cpp
@@ -3,7 +3,7 @@
 // RUN: %clang_cc1 %s -I%S -I%S/../Inputs -triple x86_64-unknown-linux-gnu -std=c++98 -O2 -disable-llvm-passes -emit-cir -o %t.opt
 // RUN: FileCheck -allow-deprecated-dag-overlap --check-prefix=CHECK-FORCE-EMIT %s < %t.opt
 
-#include "typeinfo"
+#include <typeinfo>
 
 // CHECK: cir.global{{.*}} external @_ZTV1A
 // CHECK-FORCE-EMIT-DAG: cir.global{{.*}} available_externally @_ZTV1A
